a037bc53181542bb2509ce9697b2850975d5e2d9
Vanshaj P Mohan <VanshajKumar145@gmail.com>
2024-07-16 23:12:08 +0530

    basics

--- Basics/app.js
+++ Basics/app.js
@@ -3,3 +3,8 @@ console.log("Play good 2")
 let a = 10
 let b = 20 
 console.log("Sum is:", a+b);
+//using template literals
+let xprice = 10
+let yprice = 20
+let sum = `Total price is : ${xprice + yprice} Rupees.`
+console.log(sum) 
