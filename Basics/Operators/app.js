35e5dff1bf100799fb6640427622db25800b5dc8
Vanshaj P Mohan <VanshajKumar145@gmail.com>
2024-07-17 17:41:20 +0530

    Operators

---
+++ Basics/Operators/app.js
@@ -0,0 +1,8 @@
+//conditional operators
+let age = 20;
+console.log(age > 22); //false
+console.log(age< 19); //false
+console.log(age >=20); //true
+console.log(age <= 19); //false
+console.log(age != 22); //true
+console.log(age == 20); //true
