205be1a1eddd9d2cea4f606bd6638086d71a4633
Vanshaj P Mohan <VanshajKumar145@gmail.com>
2024-07-19 00:27:20 +0530

    If else case

---
+++ Basics/Conditional Statements/app.js
@@ -0,0 +1,9 @@
+//Conditional Statement
+console.log("If statement start")
+let age = 24;
+if (age > 20){
+    console.log("Allowed to get license")
+}
+else if (age < 20){
+    console.log( "Not allowed to get license")
+}
