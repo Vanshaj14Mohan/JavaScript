5b9a95f87f07c495cf56468bcde9e9e333737e10
Vanshaj P Mohan <VanshajKumar145@gmail.com>
2024-07-20 16:21:45 +0530

    Logical Operators

---
+++ Basics/Logical Operators/app.js
@@ -0,0 +1,20 @@
+//Logical AND
+console.log("Logical And");
+let marks = 50;
+if (marks >= 35 && marks > 85){
+    console.log("Pass");
+    console.log("Grade: A+")
+}
+else{
+    console.log("Try next time..")
+}
+
+//Logical OR
+console.log("Logical Or");
+let price = 60;
+if(price === 50 || price <= 30){
+    console.log("It's a sale");
+}
+else{
+    console.log("Not a sale today");
+}
