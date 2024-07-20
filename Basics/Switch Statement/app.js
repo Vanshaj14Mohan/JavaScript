16909f910cdeded4a9dfe461345afea5dd618494
Vanshaj P Mohan <VanshajKumar145@gmail.com>
2024-07-20 17:12:15 +0530

    Switch Statement

---
+++ Basics/Switch Statement/app.js
@@ -0,0 +1,14 @@
+let color = "yellow";
+switch(color){
+    case "red":
+        console.log("Stop");
+        break;
+    case "yellow":
+        console.log("Wait");
+        break;
+    case "green":
+        console.log("Go");
+        break;
+    default:
+        console.log("Broken Light");
+}
---
+++ Basics/Switch Statement/practice.js
@@ -0,0 +1,27 @@
+//Use Switch Statement to print the day of the week using a number variable "day" with values 1-7, 1 = Monday, 2 = Tuesday....
+let day = "5";
+switch(day){
+    case "1":
+        console.log("It's Monday");
+        break;
+    case "2":
+        console.log("It's Tuesday");
+        break;
+    case "3":
+        console.log("It's Wednesday");
+        break;
+    case "4":
+        console.log("It's Thursday");
+        break;
+    case "5":
+        console.log("It's Friday");
+        break;
+    case "6":
+        console.log("It's Saturday");
+        break;
+    case "7":
+        console.log("It's Sunday");
+        break;
+    default:
+        console.log("Out of range");
+}
