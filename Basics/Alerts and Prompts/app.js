45aec1e0d2aa2adc7597dc1ecf0578648ff8ddf5
Vanshaj P Mohan <VanshajKumar145@gmail.com>
2024-07-20 23:50:17 +0530

    Alerts and Prompts

---
+++ Basics/Alerts and Prompts/app.js
@@ -0,0 +1,9 @@
+// alert("Something is wrong");
+console.log("Just an example");
+console.error("This is an error"); //will show a error 
+console.warn("This is a warning"); //will show a warning
+
+//for prompt
+let firstName = prompt("enter your first name: ");
+let secondName = prompt("enter your second name: ");
+console.log(firstName, secondName);
