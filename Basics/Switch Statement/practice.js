f55521e499a92592124c0f3c0df6245cf858295b
Vanshaj P Mohan <VanshajKumar145@gmail.com>
2024-07-20 17:18:21 +0530

    practice set

--- Basics/Switch Statement/practice.js
+++ Basics/Switch Statement/practice.js
@@ -1,25 +1,25 @@
 //Use Switch Statement to print the day of the week using a number variable "day" with values 1-7, 1 = Monday, 2 = Tuesday....
-let day = "5";
+let day = 5;
 switch(day){
-    case "1":
+    case 1:
         console.log("It's Monday");
         break;
-    case "2":
+    case 2:
         console.log("It's Tuesday");
         break;
-    case "3":
+    case 3:
         console.log("It's Wednesday");
         break;
-    case "4":
+    case 4:
         console.log("It's Thursday");
         break;
-    case "5":
+    case 5:
         console.log("It's Friday");
         break;
-    case "6":
+    case 6:
         console.log("It's Saturday");
         break;
-    case "7":
+    case 7:
         console.log("It's Sunday");
         break;
     default:
