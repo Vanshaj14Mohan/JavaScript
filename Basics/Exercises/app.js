715dfd68a931726593d4f94d27177c24273b2b88
Vanshaj P Mohan <VanshajKumar145@gmail.com>
2024-07-21 19:02:25 +0530

    Exercise

--- Basics/Exercises/app.js
+++ Basics/Exercises/app.js
@@ -23,6 +23,7 @@ else{
 //Months in Quarter 2: April, May, June
 //Months in Quarter 3: July, Aug, Sept
 //Months in Quarter 4: Oct, Nov, Dec
+console.log("Question 3")
 let quarter = 4;
 switch(quarter){
     case 1:
@@ -39,4 +40,27 @@ switch(quarter){
         break;
     default :
         console.log("Out of range");
+}
+
+//4 Write a program to find the largest of three numbers.
+console.log("Question 4")
+let a = 10, b = 40, c =50;
+if (a>b && a>c){
+    console.log("A is greatest");
+}
+else if(b>a && b>c){
+    console.log("b is greatest");
+}
+else{
+     console.log("c is greatest");
+}
+
+//5 Write a program to check if 2 numbers have the same last digit eg. 32 and 47852 have the same last digit ie.2.
+let num1 = 32;
+let num2 = 47852;
+if((num1%10)== (num2%10)){
+    console.log("Numbers have the same last digit ie.", num1%10);
+}
+else{
+    console.log("numbers don't have the same last digit");
 }
